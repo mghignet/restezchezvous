fc2d397f76361f740f2ba9268104f2a2cbb9e4e3
Maxime Ghignet <maxime.ghignet@gmail.com>
2020-04-11 17:55:15 +0200

    First button to generate a form

---
+++ src/form/form-service.ts
@@ -0,0 +1,5 @@
+import { UserInfo } from "./form.component";
+
+export function generateForm(userInfo: UserInfo) {
+    return `To be implemented! info: ${userInfo.firstName} ${userInfo.lastName}`;
+}
