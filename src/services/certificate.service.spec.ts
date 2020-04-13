01af65b6ee6c72c5ea75b49269f01308023e8914
Maxime Ghignet <maxime.ghignet@gmail.com>
2020-04-13 12:45:17 +0200

    Handle certificate expiration correctly

--- src/services/certificate.service.spec.ts
+++ src/services/certificate.service.spec.ts
@@ -1,9 +1,20 @@
 import { isExpired } from "./certificate.service";
 import { Certificate } from "../models/certificate";
+import { ReleaseReason, ReleaseReasons } from "../models/release-reason";
 
 describe("isExpired", () => {
 
-  it("should be expired when the date is prior to one hour ago", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should be expired is no release reason is provided", () => {
+
+    const expiredCertificate = buildCertificate(new Date(), []);
+    expect(isExpired(expiredCertificate)).toBe(true);
+  });
+
+  it("should expire after one hour when reason is SPORT only", () => {
 
     const sixtyOneMinutesAgo = new Date("2020-01-01T16:59:00.000Z");
     const fiftyNineMinutesAgo = new Date("2020-01-01T17:01:00.000Z");
@@ -14,19 +25,67 @@ describe("isExpired", () => {
       // @ts-ignore
       .mockImplementation(() => now);
 
-    const expiredCertificate = buildCertificate(sixtyOneMinutesAgo);
+    const expiredCertificate = buildCertificate(sixtyOneMinutesAgo, [ReleaseReasons.SPORT]);
     expect(isExpired(expiredCertificate)).toBe(true);
-    const validCertificate = buildCertificate(fiftyNineMinutesAgo);
+    const validCertificate = buildCertificate(fiftyNineMinutesAgo, [ReleaseReasons.SPORT]);
     expect(isExpired(validCertificate)).toBe(false);
   });
 
-  function buildCertificate(releaseDate: Date): Certificate {
+  it("should not be expired id date is in the future", () => {
+
+    const futureDate = new Date("2020-01-01T19:00:00.000Z");
+
+    const now = new Date("2020-01-01T18:00:00.000Z");
+    jest
+      .spyOn(Date, 'now')
+      // @ts-ignore
+      .mockImplementation(() => now);
+
+    const expiredCertificate = buildCertificate(futureDate, [ReleaseReasons.SPORT]);
+    expect(isExpired(expiredCertificate)).toBe(false);
+  });
+
+  it("should expire at the end of the day when reasons include COURSE, SANTE, FAMILLE, JUDICIAIRE but not TRAVAIL or MISSIONS", () => {
+
+    const todayDate = new Date("2020-01-02T14:00:00.000Z");
+    const yesterdayDate = new Date("2020-01-01T17:00:00.000Z");
+
+    const now = new Date("2020-01-02T18:00:00.000Z");
+    jest
+      .spyOn(Date, 'now')
+      // @ts-ignore
+      .mockImplementation(() => now);
+
+    const expiredCertificate = buildCertificate(yesterdayDate, [ReleaseReasons.SPORT, ReleaseReasons.COURSES]);
+    expect(isExpired(expiredCertificate)).toBe(true);
+    const validCertificate = buildCertificate(todayDate, [ReleaseReasons.SPORT, ReleaseReasons.COURSES]);
+    expect(isExpired(validCertificate)).toBe(false);
+  });
+
+  it("should expire 24h later when reasons include TRAVAIL or MISSIONS", () => {
+
+    const before24HoursEarlier = new Date("2020-01-01T17:59:00.000Z");
+    const after24HoursEarlier = new Date("2020-01-01T18:01:00.000Z");
+
+    const now = new Date("2020-01-02T18:00:00.000Z");
+    jest
+      .spyOn(Date, 'now')
+      // @ts-ignore
+      .mockImplementation(() => now);
+
+    const expiredCertificate = buildCertificate(before24HoursEarlier, [ReleaseReasons.COURSES, ReleaseReasons.TRAVAIL]);
+    expect(isExpired(expiredCertificate)).toBe(true);
+    const validCertificate = buildCertificate(after24HoursEarlier, [ReleaseReasons.COURSES, ReleaseReasons.TRAVAIL]);
+    expect(isExpired(validCertificate)).toBe(false);
+  });
+
+  function buildCertificate(releaseDate: Date, releaseReasons: ReleaseReason[]): Certificate {
     return {
       address: "",
       birthDate: "",
       birthLocation: "",
       creationDate: new Date(0), // irrelevant here
-      releaseReasons: [],
+      releaseReasons: releaseReasons,
       town: "",
       zipCode: "",
       releaseDate: releaseDate,
--- src/services/certificate.service.ts
+++ src/services/certificate.service.ts
@@ -1,38 +1,80 @@
-import { PDFDocument, StandardFonts } from "pdf-lib";
-import { generateQrCode } from "./qrcode.service";
-import { formatDateAndTime, formatDateOnly, formatHoursOnly, formatMinutesOnly } from "./date.service";
-import { ReleaseReasons } from "../models/release-reason";
-import { Certificate } from "../models/certificate";
+import {PDFDocument, StandardFonts} from 'pdf-lib';
+import {generateQrCode} from './qrcode.service';
+import {
+  formatDateAndTime,
+  formatDateOnly,
+  formatHoursOnly,
+  formatMinutesOnly,
+} from './date.service';
+import {ReleaseReasons} from '../models/release-reason';
+import {Certificate} from '../models/certificate';
 import rawPdfCertificate from '../assets/pdf-certificate.base64';
-import moment from "moment";
+import moment from 'moment';
 
 export function isExpired(certificate: Certificate) {
-  return moment(certificate.releaseDate).isBefore(moment().subtract(60, 'm').toDate());
+  if (certificate.releaseReasons.length === 0) return true;
+
+  if (
+    [ReleaseReasons.TRAVAIL, ReleaseReasons.MISSIONS].some((reason) =>
+      certificate.releaseReasons.includes(reason),
+    )
+  ) {
+    return moment(certificate.releaseDate).isBefore(moment().subtract(24, 'hours'));
+  }
+
+  if (
+    [
+      ReleaseReasons.COURSES,
+      ReleaseReasons.SANTE,
+      ReleaseReasons.FAMILLE,
+      ReleaseReasons.JUDICIAIRE,
+    ].some((reason) => certificate.releaseReasons.includes(reason))
+  ) {
+    return !moment(certificate.releaseDate).isSame(moment(), 'day');
+  }
+
+  if (
+    certificate.releaseReasons.length === 1 &&
+    certificate.releaseReasons.includes(ReleaseReasons.SPORT)
+  ) {
+    return moment(certificate.releaseDate).isBefore(moment().subtract(60, 'm'));
+  }
 }
 
 export async function generateCertificateAsBase64(certificate: Certificate) {
-
   const pdfDoc = await PDFDocument.load(rawPdfCertificate);
   const pages = pdfDoc.getPages();
   let page1 = pages[0];
 
   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  function drawText (text: string, x: number, y: number, size: number = 11) {
-    page1.drawText(text, { x, y, size, font })
+
+  function drawText(text: string, x: number, y: number, size: number = 11) {
+    page1.drawText(text, {x, y, size, font});
   }
 
   drawText(`${certificate.firstName} ${certificate.lastName}`, 123, 686);
   drawText(certificate.birthDate, 123, 661);
   drawText(certificate.birthLocation, 92, 638);
-  drawText(`${certificate.address} ${certificate.zipCode} ${certificate.town}`, 134, 613);
+  drawText(
+    `${certificate.address} ${certificate.zipCode} ${certificate.town}`,
+    134,
+    613,
+  );
 
-  if (certificate.releaseReasons.includes(ReleaseReasons.TRAVAIL)) drawText('x', 77, 527, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.COURSES)) drawText('x', 77, 478, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.SANTE)) drawText('x', 77, 436, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.FAMILLE)) drawText('x', 77, 400, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.SPORT)) drawText('x', 77, 345, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.JUDICIAIRE)) drawText('x', 77, 298, 19);
-  if (certificate.releaseReasons.includes(ReleaseReasons.MISSIONS)) drawText('x', 77, 260, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.TRAVAIL))
+    drawText('x', 77, 527, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.COURSES))
+    drawText('x', 77, 478, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.SANTE))
+    drawText('x', 77, 436, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.FAMILLE))
+    drawText('x', 77, 400, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.SPORT))
+    drawText('x', 77, 345, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.JUDICIAIRE))
+    drawText('x', 77, 298, 19);
+  if (certificate.releaseReasons.includes(ReleaseReasons.MISSIONS))
+    drawText('x', 77, 260, 19);
 
   drawText(certificate.town, 111, 226, 11);
 
@@ -41,7 +83,6 @@ export async function generateCertificateAsBase64(certificate: Certificate) {
     drawText(formatHoursOnly(certificate.releaseDate), 200, 201);
     drawText(formatMinutesOnly(certificate.releaseDate), 220, 201);
   }
-
 
   drawText('Date de création:', 464, 150, 7);
   drawText(formatDateAndTime(certificate.creationDate), 455, 144, 7);
