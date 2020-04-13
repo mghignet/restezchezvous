804ccab7130060425c4b5aca5cd0c5ec86f43f73
Maxime Ghignet <maxime.ghignet@gmail.com>
2020-04-13 18:27:57 +0200

    Remove certificate list, only handle one at once

--- src/repositories/certificate.repository.ts
+++ src/repositories/certificate.repository.ts
@@ -2,19 +2,17 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { Certificate } from "../models/certificate";
 
 
-export async function getCertificates() {
-  const certificates = await AsyncStorage.getItem('certificates');
+export async function getCertificate(): Promise<Certificate> {
+  const certificates = await AsyncStorage.getItem('certificate');
   // @ts-ignore
-  return JSON.parse(certificates) || [];
+  return JSON.parse(certificates);
 }
 
 export async function saveCertificate(certificate: Certificate) {
-  const certificates = await getCertificates();
-  certificates.unshift(certificate);
-  return AsyncStorage.setItem('certificates', JSON.stringify(certificates));
+  return AsyncStorage.setItem('certificate', JSON.stringify(certificate));
 }
 
 // For debugging use only
-async function deleteAllCertificates() {
-  return AsyncStorage.setItem('certificates', JSON.stringify([]));
+async function deleteCertificates() {
+  return AsyncStorage.removeItem('certificate');
 }
